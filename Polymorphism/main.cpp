a51e5930ed9b7ddc73136baeece5add6319a02d1
Mahesh Suryawanshi <Mahesh.Suryawanshi@infovision.com>
2023-05-22 16:52:23 +0530

    Polymorphism file

---
+++ Polymorphism/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class BaseClass
+{
+    int arr[10];
+};
+class DerivedBaseClass1 : public BaseClass
+{};
+class DerivedBaseClass2 : public BaseClass
+{
+};
+class DerivedClass : public DerivedBaseClass1, public DerivedBaseClass2
+{
+};
+
+    int main()
+{
+    std::cout << sizeof(DerivedClass);
+    return 0;
+}

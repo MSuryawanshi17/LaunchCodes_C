d5cfb34708eb06ff07efecbb6a7cc30f66afad51
Mahesh Suryawanshi <Mahesh.Suryawanshi@infovision.com>
2023-05-22 16:43:41 +0530

    Encapsulation File

---
+++ Encapsulation/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+class Employee
+{
+private:
+    string Name;
+    string Company;
+    int Age;
+
+public:
+    void setName(string name)
+    {
+        Name = name;
+    }
+    string getName()
+    {
+        return Name;
+    }
+
+    void setCompany(string company)
+    {
+        Company = company;
+    }
+    string getCompany()
+    {
+        return Company;
+    }
+
+    void setAge(int age)
+    {
+        Age = age;
+    }
+    int getAge()
+    {
+        return Age;
+    };
+
+public:
+    void IntroduceYourself()
+    {
+        std::cout << "Name - " << Name << std::endl;
+        std::cout << "Company - " << Company << std::endl;
+        std::cout << "Age - " << Age << std::endl;
+    }
+
+    Employee(string name, string company, int age)
+    {
+        Name = name;
+        Company = company;
+        Age = age;
+    };
+};
+
+int main()
+{
+    Employee employee1 = Employee("Rucha", "InfoVision", 24);
+
+    // employee1.Name = "Rucha";
+    // employee1.Company = "InfoVision";
+    // employee1.Age = 24;
+
+    employee1.IntroduceYourself();
+
+    Employee employee2 = Employee("Ashish", "AT&T", 23);
+
+    // employee2.Name = "Ashish";
+    //  employee2.Company = "AT&T";
+    //  employee2.Age = 23;
+
+    employee2.IntroduceYourself();
+
+    employee1.setAge(39);
+    std::cout << employee1.getName() << " is " << employee1.getAge() << " years old " << std::endl;
+
+    return 0;
+};
